88e1f961aaa1b34e9ab2493977c3c22882f4b0d7
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-13 02:16:27 +0300

    Update App.js

--- amazoncrud/src/App.js
+++ amazoncrud/src/App.js
@@ -3,12 +3,19 @@ import React from 'react';
 import './App.css';
 import Navbar from './component/Navbar';
 import Home from './component/Home'
-
+import Backoffice from './component/backOffice'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Carousel from './component/carousel'
 function App() {
   return (
     <>
+    
+    <Router>
     <Navbar />
-    <Home/>
+    < Carousel />
+    <Route path= "/" exact component={Home} />
+    <Route path= "/backOffice/" exact component={Backoffice} />
+    </Router>
     </>
   );
 }
