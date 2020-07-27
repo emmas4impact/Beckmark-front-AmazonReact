8cb7cdd953920584e3a0bbfa02290b3ee73bef65
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-27 12:18:59 +0300

    Update Home.jsx

--- amazoncrud/src/component/Home.jsx
+++ amazoncrud/src/component/Home.jsx
@@ -12,8 +12,9 @@ class Home extends Component{
         products: [],
         Totalproducts: 0,
         activePage:3,
-        rate: 0
-        
+        rate: 0,  
+        cart: [],   
+        title: "Amazon Products"   
     }
     
     componentDidMount= async()=>{
@@ -41,29 +42,46 @@ class Home extends Component{
     this.setState({activePage: pageNumber});
   }
   
-  ratingChanged = async(newRating)=>{
-    let response = await fetch("http://localhost:3002/products/",{
-        method: "GET",
+  ratingChanged = async(newRating, id)=>{
+      console.log(newRating, id)
+      //send the new review
+      //refresh thr product list
+//       console.log(newRating)
+//     let response = await fetch("http://localhost:3002/products/",{
+//         method: "GET",
       
-  })
+//   })
       
-  const data = await response.json()
+//   const data = await response.json()
 //   this.setState({
 //       rate: data.reviews.rate
 //   })
     
-    console.log(newRating)
+//     console.log(newRating)
 }
-    
+   cartHandler = async (id)=>{
+    const response = await fetch("http://localhost:3002/customers/5f0873184c8de209885350c5/add-to-cart/"+id,{
+        method: "POST",
+        
+        headers: {
+          "Content-Type": "application/json"}
+    })
+    const data = await response.json()
+    console.log(data)
+    this.setState({
+        cart: data
+    })
+} 
     
     render(){
+        console.log("carts ",this.state.cart)
         console.log(this.state.products)
         return(
             <>
                 
                 <div className="container text-center">
                
-                    <h1>Amazon CRUD PRODUCTS</h1>
+                    <h1>{this.state.title}</h1>
                     
                     <ul className="list-unstyled" id='products'>
                        
@@ -75,15 +93,16 @@ class Home extends Component{
                                 <p className="mt-0 mb-3">Description: {products.description}</p>
                                 <p className="mt-0 mb-3">Brand: {products.brand}</p>
                                 <h5 className="mt-0 mb-3">Price: {products.price}€</h5> 
-                                <p><button className="btn btn-dark addorder">Add to Cart<GiShoppingCart size={32}/></button>  </p>
-                                <p style={{display: "flex",
+                                <p><button className="btn btn-dark addorder" onClick={()=>this.cartHandler(products._id)}>Add to Cart<GiShoppingCart size={32}/></button>  </p>
+                                <div style={{display: "flex",
                                 justifyContent: "center",
                                 alignItems: "center"}}><ReactStars 
                                 count={5}
-                                onChange={this.ratingChanged}
+                                value={products.reviews.map(x => x.rate).reduce((a,v,i)=>(a*i+v)/(i+1), 0)}
+                                onChange={(e)=>this.ratingChanged(e, products._id)}
                                 size={24}
                                 isHalf={true}
-                                /></p>
+                                /></div>
                             </div>
                     </li> 
                         ))}
